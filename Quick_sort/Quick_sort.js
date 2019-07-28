9815098f99ab712b4935f316e3eaed8de8fce665
Supriyo Mondal <hisupriyooo@gmail.com>
2019-07-28 12:53:55 +0530

    Update Quick_sort.js

--- Quick_sort/Quick_sort.js
+++ Quick_sort/Quick_sort.js
@@ -15,3 +15,49 @@ function qSort(arr) {
     .concat(qSort(right));
 }
 console.log(qSort(arr)); // [1, 2, 3, 3, 4, 4, 5, 8]]
+
+//------quick sort another way-----
+
+function quickSort(arr) {
+  var left = 0;
+  var right = arr.length - 1;
+
+  return qSort(arr, left, right);
+}
+function qSort(arr, left, right) {
+  var len = arr.length,
+    pivot,
+    partitionIndex;
+
+  if (left < right) {
+    pivot = right;
+    partitionIndex = partition(arr, pivot, left, right);
+
+    //sort left and right
+    qSort(arr, left, partitionIndex - 1);
+    qSort(arr, partitionIndex + 1, right);
+  }
+
+  return arr;
+}
+function partition(arr, pivot, left, right) {
+  var pivotValue = arr[pivot],
+    partitionIndex = left;
+
+  for (var i = left; i < right; i++) {
+    if (arr[i] < pivotValue) {
+      swap(arr, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(arr, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(arr, i, j) {
+  var temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+console.log(quickSort([11, 8, 14, 3, 6, 2, 7, 0, 6])); //[0, 2, 3, 6, 6, 7, 8, 11, 14]
